9d0abf32e0c589889a436336bce276ebeab8344c
lepetinez <lepetinez@gmail.com>
2017-05-30 01:51:30 +0200

    less bus details bug fixed

--- LaboVersionOne/app/src/main/java/com/example/pc/laboversionone/LoadBusStopsDetails.java
+++ LaboVersionOne/app/src/main/java/com/example/pc/laboversionone/LoadBusStopsDetails.java
@@ -17,16 +17,9 @@ public class LoadBusStopsDetails extends AsyncTask<Object, Void, String[]> {
     protected String[] doInBackground(Object... params) {
         singleBusData = (String[]) params[0];
         stopInfo = (String) params[1];
-        try {
-            doc = Jsoup.connect(stopInfo).get();
-        } catch (IOException e1) {
-            e1.printStackTrace();
-        }
+        createDoc();
         if(doc == null){
-            singleBusData[0] = "wystapil blad. sprobuj ponownie za chwile";
-            for(int i = 0; i < 16;i++){
-                singleBusData[i+1] = "";
-            }
+            fillBusesWithNull();
             return singleBusData;
         }
         Elements newsHeadlines = doc.getElementsByTag("strong");
@@ -51,17 +44,40 @@ public class LoadBusStopsDetails extends AsyncTask<Object, Void, String[]> {
             singleBusData[1] = label2;
 
             Elements busLines = doc.getElementsByClass("komorkalinia");
-            for (int i = 0; i < 5; i++) {
-                singleBusData[i + 2] = busLines.get(i).text();
+            if(busLines.size()==4){
+                fillBusesDetailsInfoWindow(busLines,4,8);
+            }else if(busLines.size()==5){
+                fillBusesDetailsInfoWindow(busLines,5,10);
             }
-
-            Elements busDates = doc.getElementsByTag("a");
-            for (int i = 0; i < 10; i++) {
-                singleBusData[i + 7] = busDates.get(i).text();
+            else if(busLines.size() == 2){
+                fillBusesDetailsInfoWindow(busLines,2,4);
             }
-
+            else if(busLines.size() == 3){
+                fillBusesDetailsInfoWindow(busLines,3,6);
+            }
             return singleBusData;
         }
     }
-
+    public void createDoc(){
+        try {
+            this.doc = Jsoup.connect(stopInfo).get();
+        } catch (IOException e1) {
+            e1.printStackTrace();
+        }
+    }
+    public void fillBusesWithNull(){
+        singleBusData[0] = "wystapil blad. sprobuj ponownie za chwile";
+        for(int i = 0; i < 16;i++){
+            singleBusData[i+1] = "";
+        }
+    }
+    public void fillBusesDetailsInfoWindow(Elements busLines,int first, int second){
+        for (int i = 0; i < first; i++) {
+            singleBusData[i + 2] = busLines.get(i).text();
+        }
+        Elements busDates = doc.getElementsByTag("a");
+        for (int i = 0; i < second; i++) {
+            singleBusData[i + 7] = busDates.get(i).text();
+        }
+    }
 }
